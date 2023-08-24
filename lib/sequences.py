97f89dac4fd76bc5ee19f7af2f7c61af2f7fd67f
Lawi-Mwaura <Lawimwaura@gmail.com>
2023-08-24 10:13:48 +0300

    Still solving

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+    for i in range(2, length):
+        next_number = fibonacci_list[i - 1] + fibonacci_list[i - 2]
+        fibonacci_list.append(next_number)
+    print(fibonacci_list)
